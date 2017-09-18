e1882d1e1a4a00eb31d1407d0549f8b57091a367
JLALJ <mjie46@126.com>
2017-09-19 00:11:21 +0800

    no message

--- learn_models/settings.py
+++ learn_models/settings.py
@@ -118,17 +118,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(os.path.dirname(__file__),'static')
-# STATICFILES_DIRS = (
-#      os.path.join(BASE_DIR, 'static/image').replace('\\', '/'),
-# )
+# STATIC_URL = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(os.path.dirname(__file__),'static')
 STATICFILES_DIRS = (
-    ('css',os.path.join(STATIC_ROOT,'css').replace('\\','/') ),
-    ('js',os.path.join(STATIC_ROOT,'js').replace('\\','/') ),
-    ('images',os.path.join(STATIC_ROOT,'images').replace('\\','/') ),
-    ('upload',os.path.join(STATIC_ROOT,'upload').replace('\\','/') ),
+     os.path.join(BASE_DIR, 'static/image').replace('\\', '/'),
+
 )
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_DIRS = (
+#     ('css',os.path.join(STATIC_ROOT,'css').replace('\\','/') ),
+#     ('js',os.path.join(STATIC_ROOT,'js').replace('\\','/') ),
+#     ('image',os.path.join(STATIC_ROOT,'image').replace('\\','/') ),
+#     ('upload',os.path.join(STATIC_ROOT,'upload').replace('\\','/') ),
+# )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\','/')
 MEDIA_URL = '/media/'
--- learn_models/urls.py
+++ learn_models/urls.py
@@ -17,6 +17,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import staticfiles
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+
 
 from people import views
 
@@ -28,5 +32,9 @@ urlpatterns = [
     url(r'^seatDetail/', views.seatDetail),
     url(r'^testdb$', views.testdb),
     url(r'^testanimte', views.testanimte),
+    url(r'^static/(?P<path>.*)$',serve,{"document_root":settings.STATIC_ROOT}),
+
 ]
+
 # url(r'^static/(?P<path>.*)', 'django.views.static.serve', {'document_root': 'd:/wwwsite/office/static'}),
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
